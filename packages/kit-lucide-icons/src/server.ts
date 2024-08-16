54c78b455b8a418320524d95adf19992d35db95f
Allison Bellows <alibellows@gmail.com>
2024-08-16 05:51:28 -0700

    [kit-lucide-icons] fix typing (fixes next build/lucide pkg bundling error)

--- packages/kit-lucide-icons/src/server.ts
+++ packages/kit-lucide-icons/src/server.ts
@@ -2,4 +2,5 @@ import { createComponentDefinition } from '@rise-tools/react'
 
 import { Icon } from '.'
 
-export const LucideIcon = createComponentDefinition<typeof Icon>('rise-tools/kit-lucide-icons/Icon')
+type LucideIconType = typeof Icon;
+export const LucideIcon: LucideIconType = createComponentDefinition<typeof Icon>('rise-tools/kit-lucide-icons/Icon')
